ca65f4db980c0312a189c6a0118c5859de40ecbe
LinHongFeng <13729203531@163.com>
2019-08-31 09:14:47 +0800

    注释

--- src/index.js
+++ src/index.js
@@ -1,48 +1,48 @@
-// import { Header } from './Header/Header.js';
-// import createImg from './create/createImg';
-// import createBtn from './create/createBtn';
-// import createDiv from './create/createDiv';
-// import { init } from './create/lodash';
-// import style from './index.css';
-// // import "@babel/polyfill";
-// import './iconfont.css';
+import { Header } from './Header/Header.js';
+import createImg from './create/createImg';
+import createBtn from './create/createBtn';
+import createDiv from './create/createDiv';
+import { init } from './create/lodash';
+import style from './index.css';
+// import "@babel/polyfill";
+import './iconfont.css';
 
 
 
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('./service-worker.js')
-//       .then(registartion => {
-//         console.log(registartion);
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       })
-//   })
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(registartion => {
+        console.log(registartion);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  })
+}
+
+
+init();
+createImg();
+createBtn();
+createDiv();
+new Header();
+
+console.log(_.join(['a', 'b'], '***'));
+// if (module.hot) {
+//   module.hot.accept('./create/createDiv', () => {
+//     var div = document.getElementById('div-test'), root = document.getElementById('root');
+//     root.removeChild(div);
+//     createDiv();
+//   });
 // }
 
 
-// init();
-// createImg();
-// createBtn();
-// createDiv();
-// new Header();
-
-// console.log(_.join(['a', 'b'], '***'));
-// // if (module.hot) {
-// //   module.hot.accept('./create/createDiv', () => {
-// //     var div = document.getElementById('div-test'), root = document.getElementById('root');
-// //     root.removeChild(div);
-// //     createDiv();
-// //   });
-// // }
-
-
-// document.addEventListener('click', () => {
-//   import(/* webpackPrefetch: true */'./async/createDiv').then(({ handleClick }) => {
-//     handleClick();
-//   })
-// })
+document.addEventListener('click', () => {
+  import(/* webpackPrefetch: true */'./async/createDiv').then(({ handleClick }) => {
+    handleClick();
+  })
+})
 
 
 
